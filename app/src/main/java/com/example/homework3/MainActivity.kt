f9f6fae9ea026795485f0eb2864690b94d56ce97
Darya-aq <momo.darya@mail.ru>
2022-08-11 03:14:57 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework3/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.homework3
+
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<EditText>(R.id.reference).setOnEditorActionListener { v, actionId, event ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    loadImage()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadImage() {
+        val imageView = findViewById<ImageView>(R.id.image)
+        val imageURL = findViewById<EditText>(R.id.reference).text.toString()
+
+        try {
+            Picasso.get().load(imageURL).into(imageView)
+        } catch (e: Exception) {
+            Toast.makeText(this, "Wrong reference", Toast.LENGTH_SHORT).show()
+        } finally {
+            // optional finally block
+        }
+    }
+}
